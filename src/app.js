23d571dd582ad540517e9c547363a5a13c526d4a
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-06-15 18:31:49 +0530

    complete task controllers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import projectRouter from "./routes/project.routes.js";
 import projectMemberRouter from "./routes/projectmember.routes.js";
+import taskRouter from "./routes/task.routes.js";
 
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -25,5 +26,6 @@ app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/users", authRouter);
 app.use("/api/v1/projects", projectRouter);
 app.use("/api/v1/members", projectMemberRouter);
+app.use("/api/v1/tasks", taskRouter);
 
 export default app;
