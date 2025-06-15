916f1fc16ed805ace860b4557e801ec78851247a
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-06-15 18:38:03 +0530

    Complete all routes for Task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,6 +1,21 @@
 import { Router } from "express";
-import {} from "../controllers/"
+import {} from "../controllers/task.controllers.js";
+import isLoggedIn from "../middlewares/auth.middlewares";
+import { validateProjectIdAndRole } from "../middlewares/validateProject.middleware";
 
-const router = Router()
+const router = Router();
 
-export default router
+router.post(
+  "/create-task/:projectId",
+  isLoggedIn,
+  validateProjectIdAndRole,
+  createTask,
+);
+
+router.post("/get-tasks/:projectId", isLoggedIn, getTasks);
+router.post("/get-task-by-id/:taskId", isLoggedIn, getTaskById);
+router.put("/update-task/:taskId", isLoggedIn, updateTask);
+router.delete("/delete-task/:taskId", isLoggedIn, deleteTask);
+router.put("/update-task-status/:taskId", isLoggedIn, updateTaskStatus);
+
+export default router;
